1eec08f68717d57c2fcc3cf70bde9e0c100d6182
maxam2017 <whk0313@gmail.com>
2022-05-08 20:38:49 +0800

    feat: cronjob

---
+++ src/cron.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const cron = require('node-cron');
+const fetch = require('node-fetch');
+const { getDB } = require('./db');
+
+// every 5 minutes
+cron.schedule('*/5 * * * *', function () {
+  const db = getDB();
+
+  db.serialize(() => {
+    db.each(
+      'select * from record',
+      async (_, row) => {
+        const { chatId, url } = row;
+        const res = await fetch(url);
+        if (!res.ok) {
+          fetch(
+            `https://api.telegram.org/bot${process.env.TELEGRAM_ACCESS_TOKEN}/sendMessage?${new URLSearchParams({
+              chat_id: chatId,
+              text: `Oops, something went wrong in ${url}`,
+            })}`
+          );
+        }
+      },
+      () => {
+        db.close();
+      }
+    );
+  });
+});
